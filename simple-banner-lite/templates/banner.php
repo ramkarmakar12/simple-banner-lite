6557979855be84be506c99e4f47e458b90fa26c9
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2024-11-29 13:14:35 +0530

    Delete simple-banner-lite directory

--- simple-banner-lite/templates/banner.php
+++
@@ -1,6 +0,0 @@
-<?php if (!empty($banner_text)): ?>
-    <div class="ab-banner">
-        <p><?php echo esc_html($banner_text); ?></p>
-        <a href="<?php echo esc_url($banner_link); ?>" class="ab-button">Learn More</a>
-    </div>
-<?php endif; ?>
